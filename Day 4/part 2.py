eb41d25c01a93af9abead7effb1829a651819314
Necromm <46978626+Necromm@users.noreply.github.com>
2023-12-04 18:03:03 -0500

    Day 4

--- Day 4/part 2.py
+++ Day 4/part 2.py
@@ -28,7 +28,7 @@ Once all of the originals and copies have been processed, you end up with 1 inst
 Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
 '''
 
-file = open('AoCInput', 'r')
+file = open('Input', 'r')
 
 lines = file.readlines()
 totalPoints = 0
