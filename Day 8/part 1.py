f7cbba4f5124f962909abcdba5ead9a1cdbe51ce
Necromm <46978626+Necromm@users.noreply.github.com>
2023-12-10 22:13:20 -0500

    Day 8

---
+++ Day 8/part 1.py
@@ -0,0 +1,66 @@
+'''
+--- Day 8: Haunted Wasteland ---
+You're still riding a camel across Desert Island when you spot a sandstorm quickly approaching. When you turn to warn the Elf, she disappears before your eyes! To be fair, she had just finished warning you about ghosts a few minutes ago.
+
+One of the camel's pouches is labeled "maps" - sure enough, it's full of documents (your puzzle input) about how to navigate the desert. At least, you're pretty sure that's what they are; one of the documents contains a list of left/right instructions, and the rest of the documents seem to describe some kind of network of labeled nodes.
+
+It seems like you're meant to use the left/right instructions to navigate the network. Perhaps if you have the camel follow the same instructions, you can escape the haunted wasteland!
+
+After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel like AAA is where you are now, and you have to follow the left/right instructions until you reach ZZZ.
+
+This format defines each node of the network individually. For example:
+
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+Starting with AAA, you need to look up the next element based on the next left/right instruction in your input. In this example, start with AAA and go right (R) by choosing the right element of AAA, CCC. Then, L means to choose the left element of CCC, ZZZ. By following the left/right instructions, you reach ZZZ in 2 steps.
+
+Of course, you might not find ZZZ right away. If you run out of left/right instructions, repeat the whole sequence of instructions as necessary: RL really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation that takes 6 steps to reach ZZZ:
+
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
+'''
+import re
+
+file = open('Input', 'r')
+
+lines = file.readlines()
+
+DIR_ORDER = 'LR'
+
+directions = tuple(DIR_ORDER.index(x) for x in lines.pop(0).strip())
+
+nodeDict = {}
+
+
+def add_node(node):
+    curr_location, left, right = re.findall(r'[A-Z]{3}', node)
+    nodeDict[curr_location] = (left, right)
+
+
+lines.pop(0)
+for line in lines:
+    add_node(line)
+
+currNode = 'AAA'
+stepsTaken = 0
+print(currNode)
+while currNode != 'ZZZ':
+    for dir in directions:
+        currNode = nodeDict[currNode][dir]
+        print(currNode)
+        stepsTaken += 1
+        if currNode == 'ZZZ':
+            break
+
+print(stepsTaken)
